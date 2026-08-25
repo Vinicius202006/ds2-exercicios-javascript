5c0b19953e878ba8f842f44a7dd3d59e7caa5b05
Vini <vinicius.morais13@fatec.sp.gov.br>
2026-08-25 00:07:20 -0300

    feat: pratica variaveis e tipos de dados

---
+++ exercicio02.js
@@ -0,0 +1,19 @@
+const nome = "Vinicius";
+let idade = 18;
+const cidade = "São Roque";
+const matriculado = true;
+const nota = 10;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(matriculado);
+console.log(nota);
+
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(cidade));
+console.log(typeof(matriculado));
+console.log(typeof(nota));
+
+// O let é usado em valores que serão mudados(idade é um exemplo), ja o const é usado em valores que não serão alterados ao decorres do código.
