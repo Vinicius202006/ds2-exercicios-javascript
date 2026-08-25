4a51b7515fbdd9b29ba33dd37ff6c79e7c827ec4
Vini <vinicius.morais13@fatec.sp.gov.br>
2026-08-25 00:58:01 -0300

    fix: converte idade antes do calculo

--- exercicio03.js
+++ exercicio03.js
@@ -1,5 +1,7 @@
-const nome = prompt("Escreva seu nome", "nome");
+const nome = prompt("Escreva seu nome");
 const idade = prompt("idade");
 const curso = prompt("curso");
+const cidade = prompt("cidade");
 
-console.log("Olá, meu nome é" + "tenho" + "anos" + "moro em" + "e estou cursando" + [curso]);
+console.log("Olá, meu nome é " + [nome] + " tenho " + [idade] + " anos, moro em " + [cidade] + " e estou cursando " + [curso]);
+console.log(`Olá, meu nome é ${nome} tenho ${idade} anos, moro em ${cidade} e estou cursando ${curso}`);
