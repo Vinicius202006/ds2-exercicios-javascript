09cb513894b2d2ac05e46799d4c6fce30a7d8e10
Vini <vinicius.morais13@fatec.sp.gov.br>
2026-08-25 00:23:29 -0300

    feat: coleta e apresenta dados do aluno

---
+++ exercicio03.js
@@ -0,0 +1,5 @@
+const nome = prompt("Escreva seu nome", "nome");
+const idade = prompt("idade");
+const curso = prompt("curso");
+
+console.log("Olá, meu nome é" + "tenho" + "anos" + "moro em" + "e estou cursando" + [curso]);
