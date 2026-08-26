fbdb8df4ffd57233a6d8a21f56738377f9c7b10b
Vini <vinicius.morais13@fatec.sp.gov.br>
2026-08-25 23:04:54 -0300

    feat: limita tentativas de login

--- exercicio10.js
+++ exercicio10.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Processador de números</title>
-</head>
-
-<body>
-    <script>
 const login = "admin";
 const senha = "1234";
 let tentativas = 3;
@@ -39,8 +28,3 @@ if (tentativas<=0){
 }
 
 console.log("Parabéns! você entrou!");
-
-    </script>
-</body>
-
-</html>
