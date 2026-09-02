e0e40ebc8bb851c0059ab262f29d05ce306ada34
Vini <vinicius.morais13@fatec.sp.gov.br>
2026-09-02 00:59:53 -0300

    bug fix:corrigindo classificação da média final

--- js/exercicio11.js
+++ js/exercicio11.js
@@ -2,13 +2,13 @@ function somar(numero1, numero2) {
     return numero1 + numero2
 };
 
-var soma = somar(2, 3);
+var soma = somar(4, 5);
 
 console.log(`Soma: ${soma}`);
 
 function calculaMedia(nota1, nota2) {
     if (nota1 <= 10 && nota1 >= 0 && nota2 <= 10 && nota2 >= 0) {
-        return (nota1 + nota2) / 2
+        return (soma) / 2
     } else {
         console.log("Valor digitado não suportado, por favor digite um valor entre 0 e 10 nas notas");
     }
@@ -22,22 +22,22 @@ console.log(`Média: ${media}`);
 function classificaMedia(cMedia) {
 
     if (cMedia >= 7) {
-        console.log("Parabéns! passou");
+        return "Parabéns! passou";
     } else if (cMedia >= 5) {
-        console.log("Recuperação :(");
-    } else if (cMedia <= 4) {
-        console.log("Reprovado");
+        return "Recuperação";
+    } else {
+        return "Reprovado";
     }
 }
 
 
-var mediaFinal = classificaMedia(10);
+var mediaFinal = classificaMedia();
 
 console.log(mediaFinal);
 
 function saudacao(nome) {
-    console.log(`Saudações ${nome}! tudo bem com você?`);
+    return `Saudações ${nome}! tudo bem com você?`;
 }
 
-var saudar = saudacao("Vinicius");
-console.log(saudar);
+var cumprimento = saudacao("Vinicius");
+console.log(cumprimento);
